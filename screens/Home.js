4a7cb383a2e336c7af5669e6cd97a4c09341a56b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-01-09 17:18:51 -0500

    Add play Button

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { Header } from '../components'
 
 const styles = StyleSheet.create({
@@ -9,10 +9,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  play: {
+    fontSize: 45,
+    fontFamily: 'dogbyte',
+    color: '#ecf0f1',
+    marginTop: 5,
+  },
+  playIcon: {
+    height: 60,
+    width: 60,
+    marginRight: 15,
+  },
 })
+
+const PlayButton = () => {
+  const onPlayPress = () => console.log('onPlayPress event handler')
+
+  return (
+    <TouchableOpacity
+      onPress={onPlayPress}
+      style={{ flexDirection: 'row', alignItems: 'center' }}
+    >
+      <Image
+        source={require('../assets/icons/play_arrow.png')}
+        style={styles.playIcon}
+      />
+      <Text style={styles.play}>PLAY!</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default () => (
   <View style={styles.container}>
     <Header />
+    <PlayButton />
   </View>
 )
